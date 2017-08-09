89d02e72510fdfed3007f2d8fd4e232c00f28347
Ashok kumar <ashok.kumar@ashoks-MacBook-Pro-2.local>
2017-08-09 18:47:44 +0530

    Cuboid game completed

---
+++ Assets/GameCOntroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCOntroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("space")){
+			Debug.Log("Jump!");
+		}
+
+		if(Input.GetKey("left")){
+			Debug.Log("left!");
+		}
+
+		if(Input.GetKey("right")){
+			Debug.Log("right!");
+		}
+	}
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	public float CubeSpeed = 3.5f;
+	public float JumpingForce = 10.2f;
+	public float RotatingSpeed = 40.2f;
+	private bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("right")){
+			transform.RotateAround (transform.position, Vector3.up, RotatingSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey("left")){
+			transform.RotateAround (transform.position, Vector3.up, -RotatingSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKeyDown	("space") && canJump){
+			canJump = false;
+			GetComponent<Rigidbody> ().AddForce (0,JumpingForce,0); 
+		}
+
+
+		if(Input.GetKey	("up")){
+			transform.position += transform.forward * CubeSpeed * Time.deltaTime; 
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+	
+		if (collision.transform.name == "floor") {
+
+			canJump = true;
+
+		}
+
+	}
+}
